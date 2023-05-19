65dbc0d245c59fe0ae147af118d5ccd5d2783282
reistence <gvlamberti@gmail.com>
2023-05-19 17:57:48 +0200

    form fields corrected

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/generazione")
+@app.route("/gen", methods=['POST'])
 def generate():
-    return render_template('generazione.html')
+    excel_file = request.files['excelFile']
+    pdf_file = request.files['pdfFile']
+    field_nr = int(request.form['fieldNr'])
+    campo_values = [request.form.get(f'campo-{i}') for i in range(field_nr)]
+    print(excel_file, pdf_file)
+    if campo_values:
+        print(campo_values)
+    # return render_template('generazione.html')
+    return'Form submitted successfully'
