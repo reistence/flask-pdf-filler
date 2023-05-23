1982a7ed72a2044a0033ab8ec8dec927f0919d29
reistence <gvlamberti@gmail.com>
2023-05-23 15:06:47 +0200

    error handling implemented

--- app.py
+++ app.py
@@ -45,82 +45,80 @@ def index():
 # Generation
 @app.route("/gen", methods=['POST'])
 def generate():
-    # Get fields from the form via POST
-    excel_file = request.files['excelFile']
-    pdf_file = request.files['pdfFile']
-    field_nr = int(request.form['fieldNr'])
+    try:
+        # Get fields from the form via POST
+        excel_file = request.files['excelFile']
+        pdf_file = request.files['pdfFile']
+        field_nr = int(request.form['fieldNr'])
 
-    # TODO: to modify if alphabet as key
-    campo_values = [request.form.get(f'campo-{i}') for i in range(field_nr)]
-    
-    # Save and name xlxs and pdf
-    excel_filename = secure_filename(excel_file.filename)
-    pdf_filename = secure_filename(pdf_file.filename)
-    excel_file.save(os.path.join(app.config['UPLOAD_FOLDER'], excel_filename))
-    pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename))
+        # TODO: to modify if alphabet as key
+        campo_values = [request.form.get(f'campo-{i}') for i in range(field_nr)]
+        
+        # Save and name xlxs and pdf
+        excel_filename = secure_filename(excel_file.filename)
+        pdf_filename = secure_filename(pdf_file.filename)
+        excel_file.save(os.path.join(app.config['UPLOAD_FOLDER'], excel_filename))
+        pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename))
 
-    # destroy/recreate output folder
-    shutil.rmtree(app.config['OUTPUT_FOLDER'])
-    if not os.path.exists(OUTPUT_FOLDER):
-         os.makedirs(OUTPUT_FOLDER)
+        # destroy/recreate output folder
+        shutil.rmtree(app.config['OUTPUT_FOLDER'])
+        if not os.path.exists(OUTPUT_FOLDER):
+            os.makedirs(OUTPUT_FOLDER)
 
-     # Create a zip folder
-    zip_filename = excel_filename + pdf_filename +'_PDFs'
-    # zip_path = os.path.join(app.config['zip_filename'],)
+        # Create a zip folder
+        zip_filename = excel_filename + pdf_filename +'_PDFs'
+        # zip_path = os.path.join(app.config['zip_filename'],)
 
-    template_pdf = pdfrw.PdfReader('./upload/'+pdf_filename);
-    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
+        template_pdf = pdfrw.PdfReader('./upload/'+pdf_filename);
+        template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
 
-    # open the excel as worksheet
-    workbook = openpyxl.load_workbook('./upload/'+excel_filename)
-    worksheet = workbook.active
-    data = []
+        # open the excel as worksheet
+        workbook = openpyxl.load_workbook('./upload/'+excel_filename)
+        worksheet = workbook.active
+        data = []
 
-    # Read the data from the Excel file
-    for row in worksheet.iter_rows(values_only=True):
-        data.append(row)
+        # Read the data from the Excel file
+        for row in worksheet.iter_rows(values_only=True):
+            data.append(row)
 
-    # Iterate over the data
-    for i, row in enumerate(data):
-        if i == 0:  # Skip the first row
-            continue
-        nameToBeSaved = row[0]+'_'+row[1]
-         # new PDF object by copying the template
-        output_pdf = pdfrw.PdfWriter()
-        output_pdf.addpages(template_pdf.pages[:])
-
-         # Fill the form fields with the data
-        for page in template_pdf.pages:
-            annotations = page['/Annots']
-            if annotations is None:
+        # Iterate over the data
+        for i, row in enumerate(data):
+            if i == 0:  # Skip the first row
                 continue
-            for annotation in annotations:
-                
-                if annotation['/Subtype'] == '/Widget' and '/T' in annotation:
-                    field_name = annotation['/T'][1:-1]  # Remove parentheses
-                
-                    for name, value in zip(campo_values, row):
-                        if field_name == 'number':
-                            annotation.update(pdfrw.PdfDict(V=str(i), Ff=1))
-                        elif field_name == name:
-                            annotation.update(pdfrw.PdfDict(V=value, Ff=1))
+            nameToBeSaved = row[0]+'_'+row[1]
+            # new PDF object by copying the template
+            output_pdf = pdfrw.PdfWriter()
+            output_pdf.addpages(template_pdf.pages[:])
+
+            # Fill the form fields with the data
+            for page in template_pdf.pages:
+                annotations = page['/Annots']
+                if annotations is None:
+                    continue
+                for annotation in annotations:
+                    
+                    if annotation['/Subtype'] == '/Widget' and '/T' in annotation:
+                        field_name = annotation['/T'][1:-1]  # Remove parentheses
+                    
+                        for name, value in zip(campo_values, row):
+                            if field_name == 'number':
+                                annotation.update(pdfrw.PdfDict(V=str(i), Ff=1))
+                            elif field_name == name:
+                                annotation.update(pdfrw.PdfDict(V=value, Ff=1))
 
 
-        output_name = './output/' +pdf_filename + '_{}.pdf'.format(clean_file_name(nameToBeSaved))
-        #save pdf
-        output_pdf.write(output_name, template_pdf)
+            output_name = './output/' +pdf_filename + '_{}.pdf'.format(clean_file_name(nameToBeSaved))
+            #save pdf
+            output_pdf.write(output_name, template_pdf)
 
-    shutil.make_archive( zip_filename, 'zip', OUTPUT_FOLDER)
+        shutil.make_archive( zip_filename, 'zip', OUTPUT_FOLDER)
 
-    # Return the download link to the zip folder
-    # return render_template('generazione.html', zip_filename=zip_filename)
-    return send_file(os.path.basename(zip_filename + '.zip'), as_attachment=True)
+        # Return the download link to the zip folder
+        # return render_template('generazione.html', zip_filename=zip_filename)
+        return send_file(os.path.basename(zip_filename + '.zip'), as_attachment=True)
+    except Exception as error:
+        print("An error occurred:", type(error).__name__, "–", error)
+        return render_template('error.html', error=(type(error).__name__))
 
-
-
-# @app.route('/download/<filename>')
-# def download_file(filename):
-#     file_path = os.path.join(app.config['OUTPUT_FOLDER'], filename)
-#     return send_file(file_path, as_attachment=True)
 
 
